c5ca3ce968220c25323aabbef4da7627d08f54dc
JoseGerchinhoren <josegerchinhoren97@gmail.com>
2024-01-17 10:02:10 -0300

    Guarda correctamente informacion en csv

--- ingresaCombustible.py
+++ ingresaCombustible.py
@@ -10,10 +10,10 @@ from botocore.exceptions import NoCredentialsError
 # Obtener credenciales
 aws_access_key, aws_secret_key, region_name, bucket_name = cargar_configuracion()
 
-# Conecta a S3
+# Conectar a S3
 s3 = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, region_name=region_name)
 
-csv_filename = "cargas_combustible_empresa.csv"
+csv_filename = "cargasCombustible.csv"
 
 # Inicializar la lista de números de colectivo
 numeros_colectivos = [
@@ -27,38 +27,41 @@ formato_hora = '%H:%M'
 
 def guardar_carga_empresa_en_s3(data, filename):
     try:
-        # Leer el archivo CSV desde S3
+        # Leer el archivo CSV desde S3 o crear un DataFrame vacío con las columnas definidas
         try:
             response = s3.get_object(Bucket=bucket_name, Key=filename)
             df_total = pd.read_csv(io.BytesIO(response['Body'].read()))
         except s3.exceptions.NoSuchKey:
-            df_total = pd.DataFrame(columns=['idCarga', 'coche', 'fecha', 'hora', 'contadorLitrosInicio', 'contadorLitrosCierre', 'litrosCargados', 'numeroPrecintoViejo', 'numeroPrecintoNuevo', 'litrosTanqueInicioDia', 'ingresoGasoil', 'litrosTanqueFinDia', 'litrosTotalesCargadosDia', 'comentario', 'usuario'])
+            st.warning("No se encontró el archivo CSV en S3. Creando un DataFrame vacío.")
+            df_total = pd.DataFrame(columns=['idCarga', 'coche', 'fecha', 'hora', 'lugarCarga', 'contadorLitrosInicio', 'contadorLitrosCierre', 'litrosCargados', 'precio', 'numeroPrecintoViejo', 'numeroPrecintoNuevo', 'comentario', 'usuario'])
 
         # Obtener el ID de la revisión (longitud actual del DataFrame)
         id_carga = len(df_total)
 
         # Crear un diccionario con la información de la carga
-        nueva_revision = {'idCarga': id_carga,
-                        'coche': data['coche'],
-                        'fecha': data['fecha'],
-                        'hora': data['hora'],
-                        'contadorLitrosInicio': data['contadorLitrosInicio'],
-                        'contadorLitrosCierre': data['contadorLitrosCierre'],
-                        'litrosCargados': data['litrosCargados'],
-                        'numeroPrecintoViejo': data['numeroPrecintoViejo'],
-                        'numeroPrecintoNuevo': data['numeroPrecintoNuevo'],
-                        'litrosTanqueInicioDia': data['litrosTanqueInicioDia'],
-                        'ingresoGasoil': data['ingresoGasoil'],
-                        'litrosTanqueFinDia': data['litrosTanqueFinDia'],
-                        'litrosTotalesCargadosDia': data['litrosTotalesCargadosDia'],
-                        'comentario': data['comentario'],
-                        'usuario': data['usuario']}
+        nueva_carga = {
+            'idCarga': id_carga,
+            'coche': int(data['coche']),
+            'fecha': data['fecha'],
+            'hora': data['hora'],
+            'lugarCarga': data['lugarCarga'],
+            'contadorLitrosInicio': int(data.get('contadorLitrosInicio', 0)),
+            'contadorLitrosCierre': int(data.get('contadorLitrosCierre', 0)),
+            'litrosCargados': int(data.get('litrosCargados', 0)),
+            'precio': int(data.get('precio', 0)),
+            'numeroPrecintoViejo': int(data.get('numeroPrecintoViejo', 0)),
+            'numeroPrecintoNuevo': int(data.get('numeroPrecintoNuevo', 0)),
+            'comentario': data.get('comentario', ''),
+            'usuario': data['usuario']
+        }
 
-        # Convertir el diccionario en un DataFrame
-        nueva_df = pd.DataFrame([nueva_revision])
+        # Agregar información específica del lugar de carga
+        if data['lugarCarga'] == 'Surtidor':
+            nueva_carga['lugarCarga'] = 'Surtidor'
+            nueva_carga['precio'] = int(data.get('precio', 0))
 
-        # Concatenar el nuevo DataFrame con el existente
-        df_total = pd.concat([df_total, nueva_df], ignore_index=True)
+        # Actualizar el DataFrame con los valores del nuevo registro
+        df_total = pd.concat([df_total, pd.DataFrame([nueva_carga])], ignore_index=True)
 
         # Guardar el DataFrame actualizado en S3
         with io.StringIO() as csv_buffer:
@@ -74,7 +77,7 @@ def guardar_carga_empresa_en_s3(data, filename):
         st.error("Credenciales de AWS no disponibles. Verifica la configuración.")
 
     except Exception as e:
-        st.error(f"Error al guardar la información en S3: {e}")
+        st.error(f"Error al guardar la información: {e}")
         
 def main():
     st.title("Ingresar Carga de Combustible")
@@ -105,7 +108,7 @@ def main():
             'precio': precio_surtidor,
             'numeroPrecintoNuevo': numeroPrecintoNuevo_surtidor,
             'comentario': comentario_surtidor,
-            'usuario': usuario
+            'usuario': usuario,
         }
 
         # Botón para realizar acciones asociadas a "Carga en Surtidor"
@@ -141,63 +144,10 @@ def main():
             'usuario': usuario
         }
 
-        # Botón para realizar acciones asociadas a "Carga en Surtidor"
+        # Botón para realizar acciones asociadas a "Carga en Tanque"
         if st.button('Guardar Carga de Combustible en Tanque'):
             guardar_carga_empresa_en_s3(data_tanque, csv_filename)
 
-    # # Configuración inicial
-    # st.title("Ingresar Carga de Combustible en Empresa")
-
-    # usuario = st.session_state.user_nombre_apellido
-
-    # # Seleccionar el número de coche desde un selectbox
-    # coche = st.selectbox("Seleccione número de coche:", numeros_colectivos)
-
-    # fecha = obtener_fecha_argentina().strftime(formato_fecha)
-
-    # hora = obtener_fecha_argentina().strftime(formato_hora)
-
-    # contadorLitrosInicio = st.number_input('Ingrese la cantidad inicial de litros de combustible en el contador', min_value=0, value=0, step=1)
-
-    # contadorLitrosCierre = st.number_input('Ingrese la cantidad final de litros de combustible en el contador', min_value=0, value=0, step=1)
-
-    # litrosCargados = st.number_input('Ingrese la cantidad de litros cargados', min_value=0, value=0, step=1)
-
-    # numeroPrecintoViejo = st.number_input('Ingrese el numero de precinto viejo', min_value=0, value=0, step=1)
-
-    # numeroPrecintoNuevo = st.number_input('Ingrese el numero de precinto nuevo', min_value=0, value=0, step=1)
-
-    # comentario = st.text_input('Ingrese un comentario, si se desea')
-
-    # litrosTanqueInicioDia = st.number_input('Ingrese los litros del tanque al comienzo del día', min_value=0, value=0, step=1)
-
-    # ingresoGasoil = st.number_input('Ingrese los litros de Gas Oil que se cargaron en el tanque', min_value=0, value=0, step=1)
-
-    # litrosTanqueFinDia = st.number_input('Ingrese los litros del tanque al final del día', min_value=0, value=0, step=1)
-
-    # litrosTotalesCargadosDia = st.number_input('Ingrese los litros totales cargados en el día', min_value=0, value=0, step=1)
-
-    # # Crear un diccionario con la información del formulario
-    # data = {
-    #     'coche': coche,
-    #     'fecha': fecha,
-    #     'hora': hora,
-    #     'contadorLitrosInicio': contadorLitrosInicio,
-    #     'contadorLitrosCierre': contadorLitrosCierre,
-    #     'litrosCargados': litrosCargados,
-    #     'numeroPrecintoViejo': numeroPrecintoViejo,
-    #     'numeroPrecintoNuevo': numeroPrecintoNuevo,
-    #     'litrosTanqueInicioDia': litrosTanqueInicioDia,
-    #     'ingresoGasoil': ingresoGasoil,
-    #     'litrosTanqueFinDia': litrosTanqueFinDia,
-    #     'litrosTotalesCargadosDia': litrosTotalesCargadosDia,
-    #     'comentario': comentario,
-    #     'usuario': usuario
-    # }
-
-    # if st.button('Guardar Carga de Combustible'):
-    #     # Llamar a la función para guardar en S3
-    #     guardar_carga_empresa_en_s3(data, csv_filename)
 
 if __name__ == "__main__":
     main()
