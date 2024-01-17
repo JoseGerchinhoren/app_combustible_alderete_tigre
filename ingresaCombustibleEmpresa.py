4e1a2e81d49c49e58aaf2208d800a4cc2dd5d306
JoseGerchinhoren <josegerchinhoren97@gmail.com>
2024-01-16 21:55:16 -0300

    Actualizacion modulo ingresaCombustible

--- ingresaCombustibleEmpresa.py
+++ ingresaCombustibleEmpresa.py
@@ -77,59 +77,122 @@ def guardar_carga_empresa_en_s3(data, filename):
         st.error(f"Error al guardar la información en S3: {e}")
         
 def main():
-    # Configuración inicial
-    st.title("Ingresar Carga de Combustible en Empresa")
+    st.title("Ingresar Carga de Combustible")
 
     usuario = st.session_state.user_nombre_apellido
 
-    # Seleccionar el número de coche desde un selectbox
-    coche = st.selectbox("Seleccione número de coche:", numeros_colectivos)
+    # Utilizando st.expander para la sección "Carga en Surtidor"
+    with st.expander('Carga en Surtidor'):
+        coche_surtidor = st.selectbox("Seleccione número de coche:", numeros_colectivos)
+        numeroPrecintoViejo_surtidor = st.number_input('Ingrese el numero de precinto viejo', min_value=0, value=0, step=1)
+        litrosCargados_surtidor = st.number_input('Ingrese la cantidad de litros cargados', min_value=0, value=0, step=1)
+        precio_surtidor = st.number_input('Ingrese el precio de la carga en pesos', min_value=0, value=0, step=1)
+        numeroPrecintoNuevo_surtidor = st.number_input('Ingrese el numero de precinto nuevo', min_value=0, value=0, step=1)
+        comentario = st.text_input('Ingrese un comentario, si se desea')
 
-    fecha = obtener_fecha_argentina().strftime(formato_fecha)
+        # Obtener fecha y hora actual en formato de Argentina
+        fecha_surtidor = obtener_fecha_argentina().strftime(formato_fecha)
+        hora_surtidor = obtener_fecha_argentina().strftime(formato_hora)
 
-    hora = obtener_fecha_argentina().strftime(formato_hora)
+        # Crear un diccionario con la información del formulario
+        data_surtidor = {
+            'lugarCarga': 'Surtidor',
+            'coche': coche_surtidor,
+            'fecha': fecha_surtidor,
+            'hora': hora_surtidor,
+            'numeroPrecintoViejo': numeroPrecintoViejo_surtidor,
+            'litrosCargados': litrosCargados_surtidor,
+            'precio': precio_surtidor,
+            'numeroPrecintoNuevo': numeroPrecintoNuevo_surtidor,
+            'comentario': comentario,
+            'usuario': usuario
+        }
 
-    contadorLitrosInicio = st.number_input('Ingrese la cantidad inicial de litros de combustible en el contador', min_value=0, value=0, step=1)
+        # Botón para realizar acciones asociadas a "Carga en Surtidor"
+        if st.button('Guardar Carga de Combustible en Surtidor'):
+            guardar_carga_empresa_en_s3(data_surtidor, csv_filename)
 
-    contadorLitrosCierre = st.number_input('Ingrese la cantidad final de litros de combustible en el contador', min_value=0, value=0, step=1)
+    # Utilizando st.expander para la sección "Carga en Tanque"
+    with st.expander('Carga en Tanque'):
+        coche_tanque = st.selectbox("Seleccione número de coche: ", numeros_colectivos)
+        numeroPrecintoViejo = st.number_input('Ingrese el numero de precinto viejo ', min_value=0, value=0, step=1)
+        contadorLitrosInicio = st.number_input('Ingrese la cantidad inicial de litros de combustible en el contador ', min_value=0, value=0, step=1)
+        litrosCargados = st.number_input('Ingrese la cantidad de litros cargados ', min_value=0, value=0, step=1)
+        contadorLitrosCierre = st.number_input('Ingrese la cantidad final de litros de combustible en el contador ', min_value=0, value=0, step=1)
+        numeroPrecintoNuevo = st.number_input('Ingrese el numero de precinto nuevo ', min_value=0, value=0, step=1)
 
-    litrosCargados = st.number_input('Ingrese la cantidad de litros cargados', min_value=0, value=0, step=1)
+        # Crear un diccionario con la información del formulario
+        data_tanque = {
+            'lugarCarga': 'Tanque',
+            'coche': coche_tanque,
+            'fecha': fecha_surtidor,
+            'hora': hora_surtidor,
+            'numeroPrecintoViejo': numeroPrecintoViejo_surtidor,
+            'litrosCargados': litrosCargados_surtidor,
+            'precio': precio_surtidor,
+            'numeroPrecintoNuevo': numeroPrecintoNuevo_surtidor,
+            'comentario': comentario,
+            'usuario': usuario
+        }
 
-    numeroPrecintoViejo = st.number_input('Ingrese el numero de precinto viejo', min_value=0, value=0, step=1)
+        # Botón para realizar acciones asociadas a "Carga en Surtidor"
+        if st.button('Guardar Carga de Combustible en Tanque'):
+            guardar_carga_empresa_en_s3(data_tanque, csv_filename)
+        
 
-    numeroPrecintoNuevo = st.number_input('Ingrese el numero de precinto nuevo', min_value=0, value=0, step=1)
+    # # Configuración inicial
+    # st.title("Ingresar Carga de Combustible en Empresa")
 
-    comentario = st.text_input('Ingrese un comentario, si se desea')
+    # usuario = st.session_state.user_nombre_apellido
 
-    litrosTanqueInicioDia = st.number_input('Ingrese los litros del tanque al comienzo del día', min_value=0, value=0, step=1)
+    # # Seleccionar el número de coche desde un selectbox
+    # coche = st.selectbox("Seleccione número de coche:", numeros_colectivos)
 
-    ingresoGasoil = st.number_input('Ingrese los litros de Gas Oil que se cargaron en el tanque', min_value=0, value=0, step=1)
+    # fecha = obtener_fecha_argentina().strftime(formato_fecha)
 
-    litrosTanqueFinDia = st.number_input('Ingrese los litros del tanque al final del día', min_value=0, value=0, step=1)
+    # hora = obtener_fecha_argentina().strftime(formato_hora)
 
-    litrosTotalesCargadosDia = st.number_input('Ingrese los litros totales cargados en el día', min_value=0, value=0, step=1)
+    # contadorLitrosInicio = st.number_input('Ingrese la cantidad inicial de litros de combustible en el contador', min_value=0, value=0, step=1)
 
-    # Crear un diccionario con la información del formulario
-    data = {
-        'coche': coche,
-        'fecha': fecha,
-        'hora': hora,
-        'contadorLitrosInicio': contadorLitrosInicio,
-        'contadorLitrosCierre': contadorLitrosCierre,
-        'litrosCargados': litrosCargados,
-        'numeroPrecintoViejo': numeroPrecintoViejo,
-        'numeroPrecintoNuevo': numeroPrecintoNuevo,
-        'litrosTanqueInicioDia': litrosTanqueInicioDia,
-        'ingresoGasoil': ingresoGasoil,
-        'litrosTanqueFinDia': litrosTanqueFinDia,
-        'litrosTotalesCargadosDia': litrosTotalesCargadosDia,
-        'comentario': comentario,
-        'usuario': usuario
-    }
+    # contadorLitrosCierre = st.number_input('Ingrese la cantidad final de litros de combustible en el contador', min_value=0, value=0, step=1)
 
-    if st.button('Guardar Carga de Combustible'):
-        # Llamar a la función para guardar en S3
-        guardar_carga_empresa_en_s3(data, csv_filename)
+    # litrosCargados = st.number_input('Ingrese la cantidad de litros cargados', min_value=0, value=0, step=1)
+
+    # numeroPrecintoViejo = st.number_input('Ingrese el numero de precinto viejo', min_value=0, value=0, step=1)
+
+    # numeroPrecintoNuevo = st.number_input('Ingrese el numero de precinto nuevo', min_value=0, value=0, step=1)
+
+    # comentario = st.text_input('Ingrese un comentario, si se desea')
+
+    # litrosTanqueInicioDia = st.number_input('Ingrese los litros del tanque al comienzo del día', min_value=0, value=0, step=1)
+
+    # ingresoGasoil = st.number_input('Ingrese los litros de Gas Oil que se cargaron en el tanque', min_value=0, value=0, step=1)
+
+    # litrosTanqueFinDia = st.number_input('Ingrese los litros del tanque al final del día', min_value=0, value=0, step=1)
+
+    # litrosTotalesCargadosDia = st.number_input('Ingrese los litros totales cargados en el día', min_value=0, value=0, step=1)
+
+    # # Crear un diccionario con la información del formulario
+    # data = {
+    #     'coche': coche,
+    #     'fecha': fecha,
+    #     'hora': hora,
+    #     'contadorLitrosInicio': contadorLitrosInicio,
+    #     'contadorLitrosCierre': contadorLitrosCierre,
+    #     'litrosCargados': litrosCargados,
+    #     'numeroPrecintoViejo': numeroPrecintoViejo,
+    #     'numeroPrecintoNuevo': numeroPrecintoNuevo,
+    #     'litrosTanqueInicioDia': litrosTanqueInicioDia,
+    #     'ingresoGasoil': ingresoGasoil,
+    #     'litrosTanqueFinDia': litrosTanqueFinDia,
+    #     'litrosTotalesCargadosDia': litrosTotalesCargadosDia,
+    #     'comentario': comentario,
+    #     'usuario': usuario
+    # }
+
+    # if st.button('Guardar Carga de Combustible'):
+    #     # Llamar a la función para guardar en S3
+    #     guardar_carga_empresa_en_s3(data, csv_filename)
 
 if __name__ == "__main__":
     main()
